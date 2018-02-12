b88ef07723ec680f1766b09e700d060852c75c76
Mouaffakkub <mouaffak.kubaissi@ocsbstudent.ca>
2018-02-12 14:14:02 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,9 @@
+-----------------------------------------------------------------------------------------
+--
+-- Created by: Mouaffak Koubeisy
+-- Created on: Feb 12
+-- The code gets the text, colour and picture on screen
+-----------------------------------------------------------------------------------------
+
+local myText = display.newText( " Mouaffak Koubeisy ", 768, 1024, native.systemFont, 250 )
+myText:setFillColor( 0.25, 0.5, 1 )
